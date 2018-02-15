36b4114b2c97dc72a17f9f649717bc3a160b2adc
Liam McClelland <mcclelland-109773@3aaa.ac.uk>
2018-02-15 15:14:12 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,6 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "---------"
+  puts " #{board[3]} | #{board[5]} | #{board[6]} "
+  puts "---------"
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
