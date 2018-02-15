315fb9b07bd8226d219688f69958ab99dab45fb0
Liam McClelland <mcclelland-109773@3aaa.ac.uk>
2018-02-15 15:19:15 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,6 +6,11 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def move(board, index, character)
+  board[index] = character
+end
+
+
 def turn(board)
   puts "Please enter 1-9:"
 end
