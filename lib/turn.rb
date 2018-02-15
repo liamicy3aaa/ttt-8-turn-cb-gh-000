58dd06e9b4dd20e391f3b61f98aa16434b8420a6
Liam McClelland <mcclelland-109773@3aaa.ac.uk>
2018-02-15 15:24:32 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,7 +13,9 @@ end
 def valid_move(board, index)
   if index.between?(0,8)
     if board[index] == "" || board[index] == " " || board[index] == nil 
-      
+      return true 
+    else 
+      return false
   else
     return false
   end
